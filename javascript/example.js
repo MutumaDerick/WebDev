252cc28acf76b676b31e4230d218262e74597770
MutumaDerick <pdmutuma@gmail.com>
2024-07-30 15:26:59 +0300

    Web dev classes

---
+++ javascript/example.js
@@ -0,0 +1,47 @@
+//statement;
+
+a = 10;
+
+//var variable name = value;
+var b = 20;
+
+
+// let variable name = value;
+let c =15;
+
+// const variable name = value;
+const pi = 3.1452;
+
+//arithmetic operator
+//+, -, /, *, ++, --
+
+console.log(a + b + c);
+console.log(a*b);
+console.log(++a);
+
+
+//data types
+//numbers - integers, decimals
+//strings - characters
+//boolean -trues/false
+// array - list of items
+var array = ["toyota", "nissan", "volvo"];
+console.log(array);
+console.log(array[2]);
+//object
+var person = { firstName: "Derick", location: "Nairobi", course: "Web"}
+console.log(person);
+
+console.log(   person.firstName);
+
+
+function functionName(){
+    ///statements
+    ///statements
+    ///statements
+}
+function division(a, b){
+    return a/b;
+}
+
+console.log(division(100, 6));
---
+++ javascript/script.js
@@ -0,0 +1,12 @@
+heading = document.getElementById("benefit_heading");
+heading.innerHTML ="Semantic Benefits altered by JavaScript."; 
+heading.style.color = "green";
+
+
+
+
+function changeColor(element){
+    element.style.color ="green";
+}
+
+
